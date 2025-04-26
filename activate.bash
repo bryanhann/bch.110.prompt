fb0fdc1b8268be7d718bb0549799a23c0ab56683
Bryan Hann <nobody@nowhere>
2025-04-26 15:50:01 -0230

    Initial commit

---
+++ activate.bash
@@ -0,0 +1,4 @@
+source $(dirname ${BASH_SOURCE[0]})/vendor/git-prompt.sh
+source $(dirname ${BASH_SOURCE[0]})/lib/100.prompt
+source $(dirname ${BASH_SOURCE[0]})/lib/200.prompt.command
+
---
+++ lib/200.prompt.command
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Set this to execute a command with every prompt
+# export PROMPT_COMMAND='echo hello';
